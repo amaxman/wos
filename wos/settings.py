25a01a0d31e0b93e1b4da5ea0955285f23d663a9
陶亚军 <7887652@qq.com>
2025-06-09 11:12:24 +0800

    迁移数据库至本目录

--- wos/settings.py
+++ wos/settings.py
@@ -15,7 +15,7 @@ from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASES_DIR = BASE_DIR.parent / 'databases'
+DATABASES_DIR = BASE_DIR / 'databases'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
